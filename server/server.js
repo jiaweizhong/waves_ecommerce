a9339f00c60604321ab261b45c88090c5d9ab2ed
Jiawei Zhong <jiawei@jiawei-PC>
2019-03-20 23:10:53 -0400

    User/Woods/Brands Models

---
+++ server/server.js
@@ -0,0 +1,137 @@
+const express = require('express');
+// body parser for json
+const bodyParser = require('body-parser');
+// cookie parser
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+// loads env var from .env into process.env
+require('dotenv').config();
+
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+// middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// models
+const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
+
+// middleware
+const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
+//====================================
+//           Woods
+//====================================
+app.post('/api/product/wood', auth, admin, (req, res) => {
+    const wood = new Wood(req.body);
+    wood.save((err, doc) => {
+        if(err) return res.json({success:false, err});
+        res.status(200).json({
+            success: true,
+            wood: doc
+        });
+    });
+});
+
+app.get('/api/product/woods', (req, res) => {
+    Wood.find({}, (err, woods) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).send(woods);
+    });
+})
+
+//====================================
+//           BRAND
+//====================================
+
+app.post('/api/product/brand', auth, admin, (req, res) => {
+    const brand = new Brand(req.body);
+
+    brand.save((err, doc) => {
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            brand: doc
+        })
+    });
+});
+
+app.get('/api/product/brands', (req, res) => {
+    Brand.find({},(err, brands) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).send(brands);
+    });
+})
+
+//====================================
+//           USERS
+//====================================
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    });
+})
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if(err) return res.json({success:false, err});
+        res.status(200).json({
+            success: true,
+            // userdata: doc
+        })
+    });
+});
+
+app.post('/api/users/login', (req, res) => {
+    // find the email
+    User.findOne({'email':req.body.email}, (err, user) => {
+        if(!user) return res.json({loginSuccess: false, message: 'Auth failes, email not found'});
+        // check the password if email exist
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({loginSuccess: false, message: 'Wrong password'});
+
+            // generate new token
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                });
+            });
+        });
+    });
+});
+
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.user._id},
+        {token: ''},
+        (err, doc) => {
+            if(err) return res.json({success:false, err});
+            return res.status(200).send({
+                success: true
+            });
+        }
+    );
+})
+
+const port = process.env.PORT || 3002;
+
+// server listen to port
+app.listen(port, () => {
+    console.log(`The server is running at port:${port}`);
+});

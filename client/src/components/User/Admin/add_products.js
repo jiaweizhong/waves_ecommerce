db86de1d83e973b7d6f030c5b7d7786eefe9a5a8
Jiawei Zhong <jiawei@jiawei-PC>
2019-04-11 00:25:13 -0400

    Admin Add Product

--- client/src/components/User/Admin/add_products.js
+++ client/src/components/User/Admin/add_products.js
@@ -1,13 +1,284 @@
 import React, { Component } from 'react';
+import UserLayout from '../../../hoc/user';
+import FormField from '../../utils/Form/formfield';
+import { update, generateData, isFormValid, populateOptionFields } from '../../utils/Form/formActions';
+import { connect } from 'react-redux';
+import { getBrands, getWoods } from '../../../actions/products_actions';
 
 class AddProduct extends Component {
+
+    state = {
+        formError: false,
+        formSuccess: false,
+        formData: {
+            name: {
+                element: 'input',
+                value: '',
+                config: {
+                    label: 'Product Name',
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Enter your name'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            description: {
+                element: 'textarea',
+                value: '',
+                config: {
+                    label: 'Product Description',
+                    name: 'description_input',
+                    type: 'text',
+                    placeholder: 'Enter your description'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            price: {
+                element: 'input',
+                value: '',
+                config: {
+                    label: 'Product Price',
+                    name: 'price_input',
+                    type: 'number',
+                    placeholder: 'Enter your price'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            brand: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Product Brand',
+                    name: 'brands_input',
+                    options: []
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            shipping: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Shipping',
+                    name: 'shipping_input',
+                    options: [
+                        { key: true, value: 'Yes' },
+                        { key: false, value: 'No' }
+                    ]
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            available: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Available in Stock',
+                    name: 'available_input',
+                    options: [
+                        { key: true, value: 'Yes' },
+                        { key: false, value: 'No' }
+                    ]
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            wood: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Wood Material',
+                    name: 'wood_input',
+                    options: []
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            frets: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Frets',
+                    name: 'frets_input',
+                    options: [
+                        { key: 21, value: 21 },
+                        { key: 22, value: 22 },
+                        { key: 23, value: 23 },
+                        { key: 24, value: 24 }
+                    ]
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            },
+            publish: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Publish',
+                    name: 'publish_input',
+                    options: [
+                        { key: true, value: 'Public' },
+                        { key: false, value: 'Hidden' }
+                    ]
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+                showlabel: true
+            }
+        }
+
+    }
+
+    updateFields = (newFormData) => {
+        this.setState({
+            formData: newFormData
+        })
+    }
+
+    componentDidMount(){
+        const formdata = this.state.formData;
+        // populate brands
+        this.props.dispatch(getBrands()).then( response => {
+            // poplulate all the available options
+            const newFormData = populateOptionFields(formdata, this.props.products.brands,'brand');
+            // update the fields with options in the form
+            this.updateFields(newFormData);
+        })
+        // populate woods
+        this.props.dispatch(getWoods()).then( response => {
+            // poplulate all the available options
+            const newFormData = populateOptionFields(formdata, this.props.products.woods,'wood');
+            // update the fields with options in the form
+            this.updateFields(newFormData);
+        })
+    }
+
     render() {
         return (
-            <div>
-                Add Product
-            </div>
+            <UserLayout>
+                <div>
+                    <h1>Add product</h1>
+                    <form onSubmit={(event) => this.submitForm(event)}>
+                        <FormField
+                            id={'name'}
+                            formdata={this.state.formData.name}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <FormField
+                            id={'description'}
+                            formdata={this.state.formData.description}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <FormField
+                            id={'price'}
+                            formdata={this.state.formData.price}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <div className="form_devider"></div>
+                        <FormField
+                            id={'brand'}
+                            formdata={this.state.formData.brand}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <FormField
+                            id={'shipping'}
+                            formdata={this.state.formData.shipping}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <FormField
+                            id={'available'}
+                            formdata={this.state.formData.available}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <div className="form_devider"></div>
+                        <FormField
+                            id={'wood'}
+                            formdata={this.state.formData.wood}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <FormField
+                            id={'frets'}
+                            formdata={this.state.formData.frets}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        <div className="form_devider"></div>
+                        <FormField
+                            id={'publish'}
+                            formdata={this.state.formData.publish}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        {this.state.formSuccess ?
+                            <div className='form_success'>
+                                Success..
+                            </div>
+                            : null}
+
+                        {this.state.formError ?
+                            <div className='error_label'>
+                                Please check your data
+                            </div>
+                            : null}
+                        <button onClick={(event) => this.submitForm(event)}>
+                            Add product
+                        </button>
+                    </form>
+                </div>
+            </UserLayout>
         );
     }
 }
 
-export default AddProduct;
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(AddProduct);

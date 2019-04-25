b0cc07877d9bdf6609ef408d10de9a4055f568df
Jiawei Zhong <jiawei@jiawei-PC>
2019-04-24 23:04:34 -0400

    Fix typo

--- client/src/components/Header_Footer/Header/index.js
+++ client/src/components/Header_Footer/Header/index.js
@@ -13,7 +13,7 @@ class Header extends Component{
                 public:true
             },
             {
-                name:'Guitar',
+                name:'Product',
                 linkTo:'/shop',
                 public:true
             }
